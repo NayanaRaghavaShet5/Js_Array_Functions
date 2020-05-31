8fae1c78a23ed85209c6cdbdca9b89a80d7d3b46
NayanaRaghavaShet5 <51402931+NayanaRaghavaShet5@users.noreply.github.com>
2020-05-31 19:22:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,41 @@
+import emojipedia from "./emojipedia";
 var numbers = [3, 56, 2, 48, 5];
 
 //Map -Create a new array by doing something with each item in an array.
+const filMap = numbers.map(num => num * 10);
+console.log(filMap);
 
+function double(x) {
+  return x + 2;
+}
+const sec = numbers.map(double);
+console.log(sec);
+
+var emp = [];
+numbers.forEach(function(x) {
+  emp.push(x);
+});
+
+console.log(emp);
 //Filter - Create a new array by keeping the items that return true.
+
+const fil = numbers.filter(x => x > 10);
+console.log(fil);
 
 //Reduce - Accumulate a value by doing something to each item in an array.
 
+const sum = numbers.reduce((acc, iniVal) => {
+  return acc + iniVal;
+});
+console.log(sum);
 //Find - find the first item that matches from an array.
-
+const data = numbers.find(x => x == 2);
+console.log(data);
 //FindIndex - find the index of the first item that matches.
+const abc = numbers.findIndex(x => x == 2);
+console.log(abc);
+
+const mean = emojipedia.map(x => {
+  return x.meaning.substring(0, 100);
+});
+console.log(mean);
